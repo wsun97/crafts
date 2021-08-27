0a9fe112585ec74e7d1fba5a8382ae86500908a7
wsun97 <bomnaleunganda97@gmail.com>
2021-08-27 18:27:21 +0900

    main 화면 완성

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,41 +1,63 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import mainImg from '../assets/img/mainImg.jpg';
+import Navigation from '../components/Navigation';
+import UserNavigation from '../components/UserNavigation';
+import Footer from '../components/Footer';
+
+const FullContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: felx-start;
+	height: 70rem;
+`;
 
 const MainContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
   align-items: center;
+	width: 50%;
+	height : 50rem;
 `;
 
 const ImgContainer = styled.div`
-	background-color : blue;
-	display: flex;
-	flex-direction: column;	
+	// width: 100%;
+	// object-fit: contain
+
 `;
 
-const MainImg = styled.img`
-	width: 50rem;
-	height: 65rem;
+const Img = styled.img`
+
 `;
 
-const TitleContainer = styled.div`
-	display: flex;
-	flex-direction: column;
+const TitleContainer = styled.div`	
 	font-size: 2rem;
+	margin-top: 2rem;
+	margin-bottom: 1rem;
+	font-family: Times, "Times New Roman", Georgia, serif;
 `;
 
 const Main = () => {  
     return(
-        <MainContainer>
+			<Fragment>
+				<FullContainer>
+				<Navigation></Navigation>
+				<MainContainer>
 					<ImgContainer>
-						<MainImg src={mainImg}/>
+						<Img src={mainImg} alt=""/>
 					</ImgContainer>
 					<TitleContainer>
-						Jeon Hee Eun, Pottery Artist
+						Jun Hee Eun, Pottery Artist
 					</TitleContainer>
+					<Footer></Footer>
 				</MainContainer>
+				<UserNavigation></UserNavigation>
+			</FullContainer>     
+			
+			</Fragment>
+			
     );
 }
 
--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from './pages/main';
 import Store from './pages/store';
-
+import Participants from './pages/participants';
+import Contact from './pages/contact';
+import WorkProcess from './pages/work-process';
 
 const router = () => {
   return(
     <BrowserRouter>
       <Route exact path="/" component={Main}></Route>
 			<Route exact path="/store" component={Store}></Route>
+			<Route exact path="/work-process" component={WorkProcess}></Route>
+			<Route exact path="/participants" component={Participants}></Route>
+			<Route exact path="/contact" component={Contact}></Route>
     </BrowserRouter>
   )
 }
